b32ac81faa67b14ec9f7081fe217d707767c8fb0
Ding5454 <domingorodriguez2012@gmail.com>
2019-11-20 03:16:34 -0600

    Initial commit again.

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,30 @@
+// from data.js
+var tableData = data;
+
+console.log(tableData);
+
+ //use userInput as search parameter for functs
+ //iterate over array and do comparison
+ //return new result array to 
+
+function findDate(tableData, userInput){
+   
+
+    tableData.filter(tableData.datetime);
+}
+
+function findCity(tableData){
+    tableData.filter(tableData.city);
+}
+
+function findState(tableData){
+    tableData.filter(tableData.state);
+}
+
+function findCountry(tableData){
+    tableData.filter(tableData.country);
+}
+
+function findShape(tableData){
+    tableData.filter(tableData.shape);
+}
